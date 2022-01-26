c52eda069948050576887f1aadb346edeb3623db
pablogineli <64933389+pablogineli@users.noreply.github.com>
2022-01-26 16:24:46 -0300

    Created route for account creation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-const express = requeri("express");
+const express = require("express");
+const {v4: uuidV4} = require('uuid')
 
 const app = express();
+app.use(express.json())
 
+const custumers = [];
 
+/**
+ * CPF - strinf
+ * name - string
+ * id - UUiD
+ * statement []
+ */
+app.post('/account', (request, response)=>{
+    const { cpf, name } = request.body;
+    const id = uuidV4();
+
+    custumers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    })
+
+    return response.status(201).send();
+})
 
 app.listen(3333)
