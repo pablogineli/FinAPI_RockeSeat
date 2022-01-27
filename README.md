b3f0e8870b22aa66c4305dee66b4e342f0583643
pablogineli <64933389+pablogineli@users.noreply.github.com>
2022-01-26 22:22:57 -0300

    added new routes

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 ### Requisitos
 
-- [] Deve ser possivel criar um conta
-- [] Deve ser possivel buscar o extrato bancaria do cliente
-- [] Deve ser possivel realizar um depósito
-- [] Deve ser possivel realizar um saque
+- [x] Deve ser possivel criar um conta
+- [x] Deve ser possivel buscar o extrato bancaria do cliente
+- [x] Deve ser possivel realizar um depósito
+- [x] Deve ser possivel realizar um saque
 - [] Deve ser possivel buscar o extrato bancario do cliente por data
 - [] Deve ser possivel atulizar dados da conta do cliente
 - [] Deve ser possivel obter dados da conta do cliente
@@ -13,9 +13,9 @@
 
 ## Regras de negócio
 
-- [] Não deve ser possivel cadastrar uma conta com o cpf já existente
-- [] Não deve ser possivel fazer depósito em uma conta não existente
-- [] Não deve ser possivel buscar extrato em uma conta não existente
-- [] Não deve ser possivel fazer saque em uma conta não existente
+- [x] Não deve ser possivel cadastrar uma conta com o cpf já existente
+- [x] Não deve ser possivel fazer depósito em uma conta não existente
+- [x] Não deve ser possivel buscar extrato em uma conta não existente
+- [x] Não deve ser possivel fazer saque em uma conta não existente
 - [] Não deve ser possivel excluir uma conta não existente
 - [] Não deve ser possivel fazer saque quando o saldo for insuficiente
--- src/index.js
+++ src/index.js
@@ -6,8 +6,35 @@ app.use(express.json())
 
 const custumers = [];
 
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next){
+    const {cpf} = request.headers;
+    const custumer =  custumers.find((custumer)=> custumer.cpf === cpf);
+
+    if (!custumer){
+        return response.status(400).json({error: "Customer not found"});
+    }
+    //Repassa as informações do Headers para as rotas
+    request.customer = custumer;
+
+    // Continua a aplicação
+    return next();
+}
+
+function getBalance(statement){
+    const balance = statement.reduce((acc, operation)=>{
+        if (operation.type === "credit"){
+            return acc + operation.amount;
+        }else {
+            return acc - operation.amount;
+        }
+    }, 0)
+
+    return balance;
+}
+
 /**
- * CPF - strinf
+ * CPF - string
  * name - string
  * id - UUiD
  * statement []
@@ -29,6 +56,57 @@ app.post('/account', (request, response)=>{
     })
 
     return response.status(201).send();
+})
+
+/**
+ * Usando a MiddleWare de altenticação dessa forma todas as rotas a baixa do codigo vão passar por ela!
+ *
+ *          app.use(verifyIfExistsAccountCPF)
+ */
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response)=>{
+    const {customer} = request;
+
+    return response.json(customer.statement)
+})
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response)=>{
+    const { description, amount} = request.body;
+    const {customer } = request
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send();
+
+
+})
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response)=>{
+    const {amount}= request.body;
+    const {customer} = request
+
+    const balance = getBalance(customer.statement)
+
+    if (balance < amount){
+        return response.status(400).json({error : "insufficient founds!"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send()
 })
 
 app.listen(3333)
